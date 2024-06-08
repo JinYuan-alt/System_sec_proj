9a51ae366a7eb7bf448b74359acf4a2132138b6f
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-06-08 19:40:41 +0800

    Push password change commments

--- password-change-after-date.py
+++ password-change-after-date.py
@@ -1,21 +1,29 @@
 import time
 
 
-def passwd():
+#make sure this executes when user registers and creates the passwd
+
+time_list=[]
+
+def register():
    CT=time.localtime()
    yr=str(CT.tm_year)
    day=str(CT.tm_mday)
    month=str(CT.tm_mon)
-   print(yr,day,month)
-   time_list=[2024,6,1]
+   time_list=[yr,int(month)+3,day]
    P_yr=str(time_list[0])
    P_month=str(time_list[1])
    P_day=str(time_list[2])
+   #write some sql code to store this into database under user's username
+   expiry(P_yr,P_day,P_month,yr,day,month)
+
+def expiry(P_yr,P_day,P_month,yr,day,month):
+   #this should retrieve the data from sql
    if P_yr == yr and P_month == month:
       if P_day == day:
         print("yes")
 
-passwd()
+register()
 
 #make sure code to get localtime() runs once the user opens tab
 #so comparison can take place.
