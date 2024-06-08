364253afba190bce3e254aa3de16b23e947d821e
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-06-08 10:53:30 +0800

    Push prototype for session id

--- Password-lockout.py
+++ Password-lockout.py
@@ -8,14 +8,19 @@ app=Flask(__name__)
 app.secret_key='Crazy_Willy_Jones'
 
 letters = string.ascii_lowercase
-final_str=[]
+final_str = []
+seshid=""
 beta_str = ''.join(random.choice(letters) for i in range(8))
 for i in range(len(beta_str)):
-    final_str.append(i)
-    if i+1%2!=0:
-        final_str[i+1]=random.randint(0,9)
+    final_str.append(beta_str[i])
+for i in range(len(final_str)):
+    if (i + 1) % 2 == 0 and final_str[i] != final_str[-1]:
+        final_str.insert(random.randint(0, 9),i)
+for i in range(len(final_str)):
+    temp=final_str[i]
+    seshid=seshid+str(temp)
 
-print("Random string of length",8,"is:", beta_str)
+print("Random string of random length","is:", seshid)
 
 @app.route("/")
 def first():
