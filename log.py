92156dce6815a1fc2dd9f128a6c7302d8a967fd8
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-06-18 16:13:48 +0800

    comments

--- log.py
+++ log.py
@@ -1,5 +1,13 @@
-def log():
+def log(i):
     f = open("logfile.txt", "a")
-    f.write("")
+    f.write(i)
     f.write("\n")
     f.close()
+
+def login():
+    i=0
+    a=False
+    while a == False: #while password for user != password entered, i+=1
+        i+=1
+        a = True
+    log(i)
