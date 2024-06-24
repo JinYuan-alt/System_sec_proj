d9261765926ca7871da488d2f590d7563046ed99
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-06-24 13:20:16 +0800

    changes

--- Password-lockout.py
+++ Password-lockout.py
@@ -7,20 +7,6 @@ app=Flask(__name__)
 
 app.secret_key='Crazy_Willy_Jones'
 
-letters = string.ascii_lowercase
-final_str = []
-seshid=""
-beta_str = ''.join(random.choice(letters) for i in range(8))
-for i in range(len(beta_str)):
-    final_str.append(beta_str[i])
-for i in range(len(final_str)):
-    if (i + 1) % 2 == 0 and final_str[i] != final_str[-1]:
-        final_str.insert(random.randint(0, 9),i)
-for i in range(len(final_str)):
-    temp=final_str[i]
-    seshid=seshid+str(temp)
-
-print("Random string of random length","is:", seshid)
 
 @app.route("/")
 def first():
