5afae4b393bc0f23441fda24d1a4e22986d6ee94
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-07-03 21:04:06 +0800

    image scan #2

--- malicious image.py
+++ malicious image.py
@@ -1,2 +1,25 @@
-import os
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+img=Image.open(r'me.jpg')
+#just for testing
+# open the image
+
+
+# extracting the exif metadata
+exifdata = img.getexif()
+
+# looping through all the tags present in exifdata
+for tagid in exifdata:
+    # getting the tag name instead of tag id
+    tagname = TAGS.get(tagid, tagid)
+
+    # passing the tagid to get its respective value
+    value = exifdata.get(tagid)
+
+    # printing the final result
+    print(f"{tagname:25}: {value}")
+
+    if "<" or ">" in tagname:
+        tagname.replace(" ")
 
