f3f33530ad41d201e1e28080525d3fde7c423283
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-07-08 22:47:41 +0800

    image scan #2

--- Password-lockout.py
+++ Password-lockout.py
@@ -1,15 +1,21 @@
 passwd=['Nooun90']
 
 from flask import Flask, render_template, session, url_for, redirect, request, sessions
-import random, string
+import uuid, datetime
 
 app=Flask(__name__)
 
 app.secret_key='Crazy_Willy_Jones'
 
+app.config['temp']= datetime.timedelta(days=1)
+
+
+
 
 @app.route("/")
 def first():
+    myuuid = str(uuid.uuid4())
+    session['temp'] = "temp" + myuuid
     return render_template("login.html")
 
 @app.route("/",methods=['GET',"POST"])
@@ -17,8 +23,11 @@ def login():
     password=request.form.get("password")
     username=request.form.get("username")
     if password == passwd[0]:
-        return render_template("home.html")
-    return render_template("login.html")
+        session.pop('temp', None)
+        perma=str(uuid.uuid4())
+        session['temp']=username+password+perma
+        return render_template("home.html", sesh_id=session['temp'])
+    return render_template("login.html", temp_sesh=session['temp'])
 
 
 @app.route("/home")
